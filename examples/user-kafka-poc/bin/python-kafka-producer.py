34840d98416efa72819b1d887dacf8729296e460
Chris McConnell <chris.mcconnell@candidpartners.com>
2021-03-08 09:47:34 -0700

    updated readme and python script comment need to resolve connectivity issues

--- examples/user-kafka-poc/bin/python-kafka-producer.py
+++ examples/user-kafka-poc/bin/python-kafka-producer.py
@@ -3,6 +3,14 @@
 from kafka import KafkaProducer
 
 brokers = ['samplekafka-senior-b7bb.aivencloud.com:26096']
+#brokers = ['34.78.237.191:26096']
+
+# telnet samplekafka-senior-b7bb.aivencloud.com 26096
+# Trying 34.78.56.69...
+# ^C
+# cmcc@cmcc: bin $ telnet 34.78.237.191 26096
+# Trying 34.78.237.191...
+# ^C
 
     #bootstrap_servers="samplekafka-senior-b7bb.aivencloud.com:26096",
     #bootstrap_servers="https://avnadmin:hnlh4jllvd9658h5@samplekafka-senior-b7bb.aivencloud.com:26088"
@@ -10,10 +18,14 @@ brokers = ['samplekafka-senior-b7bb.aivencloud.com:26096']
 producer = KafkaProducer(
     bootstrap_servers=brokers, 
     #api_version=(0, 11, 5),
+    # ref: https://python.developreference.com/article/10948208/Cannot+connect+to+Kafka+from+Flask+in+a+dockerized+environement
     #api_version=(0, 10, 1),
     # trying some hardcoded versions per https://github.com/dpkp/kafka-python/blob/master/kafka/producer/kafka.py#L393
     #api_version=(0, 8, 2),
     #api_version=(2, 1, 0),
+    # https: // github.com/dpkp/kafka-python/issues/1308
+    # https://github.com/dpkp/kafka-python/issues/1308#issuecomment-469227827
+    request_timeout_ms=3000,
     # https://readthedocs.org/projects/kafka-python/downloads/pdf/master/
     api_version_auto_timeout_ms=3000,
     security_protocol="SSL",
